2c4ef56ee57e2bf6a81456aacaf9bac9662d6449
pavelusov79 <pavel.usov79@gmail.com>
2021-08-02 09:40:26 +1000

    change settings.py

--- it_portal/settings.py
+++ it_portal/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'it_portal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -127,6 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
